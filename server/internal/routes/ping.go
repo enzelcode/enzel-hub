201a278b1327245cc166d84e4ed61e7349ae0ff6
leonardofrizzi <leofrizzi15@gmail.com>
2025-07-21 18:35:25 -0300

    feat: implement JWT auth middleware and private route test

--- server/internal/routes/ping.go
+++ server/internal/routes/ping.go
@@ -1,13 +1,19 @@
 package routes
 
 import (
+	"github.com/enzelcode/enzel-hub/internal/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
-func PingRoute(router fiber.Router) {
-	router.Get("/ping", func(c *fiber.Ctx) error {
+func PingRoute(app *fiber.App) {
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"message": "pong"})
+	})
+
+	app.Get("/private", middleware.AuthMiddleware, func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"message": "pong",
+			"message": "rota protegida",
+			"user":    c.Locals("user_email"),
 		})
 	})
 }
